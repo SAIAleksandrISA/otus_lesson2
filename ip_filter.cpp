f5b8440b6b487199fedd283038101620311710ba
KLMN\lutovin <aleksandr.lutovin@c3dlabs.com>
2026-06-08 21:11:48 +0300

    test

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -1,4 +1,4 @@
-#include "ip_filter.h"
+#include <ip_filter.h>
 #include <cassert>
 #include <cstdlib>
 #include <algorithm>
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "ip_filter.h"
+#include <ip_filter.h>
 #include <fstream>
 #include <iostream>
 #include <algorithm>
@@ -7,17 +7,17 @@ int main(int argc, char const *argv[])
 {
     try
     {
-        std::ifstream file("..//ip_filter.tsv"); 
+        //std::ifstream file("..//ip_filter.tsv"); 
         //std::ifstream file("..//output.txt"); 
-        if (!file.is_open()) 
-        {
-            std::cerr << "Ошибка открытия файла!" << std::endl;
-            return 1;
-        }
+        //if (!file.is_open()) 
+        //{
+        //    std::cerr << "Ошибка открытия файла!" << std::endl;
+        //    return 1;
+        //}
 
         std::vector<std::vector<int>> vIP;
 
-        for(std::string line; std::getline( file, line);)
+        for(std::string line; std::getline(std::cin, line);)
         {
             auto vLine = splitLine(line, '\t');
             vIP.push_back(splitPart(vLine.at(0), '.'));
